c9a0afee5447a4c375c330bc0f0a33f6bb8402a0
gusta <gustavomullerdesouza@gmail.com>
2024-08-31 16:48:44 -0300

    Atividades

---
+++ src/Atividade02Condicoes.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Atividade02Condicoes {
+
+    public static void main(String[] args) {
+
+        int nota = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um nota: " );
+        nota = sc.nextInt();
+
+        if (nota >= 9 )
+            System.out.println("Excelente");
+        else if (nota >= 7)
+            System.out.println("Bom");
+        else if (nota >= 5)
+            System.out.println("Satisfatório");
+        else if (nota < 5) {
+            System.out.println("Insatisfatório");
+
+        }
+
+    }
+}
