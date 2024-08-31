d99fba4e3fde461f9d8a3b638e4083cfd23c4b59
gusta <gustavomullerdesouza@gmail.com>
2024-08-31 16:47:36 -0300

    Atividades

---
+++ src/Atividade01Condicoes.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Atividade01Condicoes {
+    public static void main(String[] args) {
+
+        int idade = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite sua idade : " );
+        idade = sc.nextInt();
+
+        if(idade > 17)
+            System.out.println("Essa pessoa é de maior ");
+
+        else
+            System.out.println("Essa pessoa é de menor ");
+
+
+    }
+
+}
