64a90da6e946b153e4d748f04eee3af366512d8a
gusta <gustavomullerdesouza@gmail.com>
2024-08-31 23:10:22 -0300

    Atividades

---
+++ src/Atividade06Condicoes.java
@@ -0,0 +1,24 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Scanner;
+
+public class Atividade06Condicoes {
+    public static void main(String[] args) {
+
+        double mediaFinal;
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Qual a media final: ");
+        mediaFinal = entrada.nextDouble();
+        
+        if (mediaFinal >= 7) {
+            System.out.println("Aprovado");
+        } else if (mediaFinal >= 5 && mediaFinal < 7) {
+            System.out.println("Recuperação");
+        }else if (mediaFinal < 5) {
+            System.out.println("Reprovado");
+        }
+
+
+    }
+}
